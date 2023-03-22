d38f437a6aa61b1442eb7a2f97a5c5a14f9e4a3f
Max Huecksteadt <mhueck2@pdx.edu>
2023-03-22 10:19:17 -0700

    Dockerized and everything working (forecast icon names added)

--- backend/microservice/micro/micro/settings.py
+++ backend/microservice/micro/micro/settings.py
@@ -131,4 +131,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = ["http://localhost:5173"]
+CORS_ORIGIN_WHITELIST = ["http://localhost:5173", "http://localhost:88"]
--- frontend/src/Components.tsx
+++ frontend/src/Components.tsx
@@ -31,10 +31,10 @@ export const Forecasts = () => {
                 <h3>{loc.name}</h3>
                 <ul key={loc.id} className="forecastList">
 
-                    {loc.icons.map((icon, index) =>
+                    {loc.icons.map((icon: {icon: string, period: string}, index) =>
                         <figure className="figure">
-                            <img src={icon} className="icon"/>
-                            <figcaption className="caption">Period {index}</figcaption>
+                            <img src={icon.icon} className="icon"/>
+                            <figcaption className="caption">{icon.period}</figcaption>
                         </figure>)}
                 </ul>
                 </div>
@@ -43,67 +43,3 @@ export const Forecasts = () => {
     );
 }
 
-// export const Icons = () => {
-//     const [icons, setIcons] = useState([]);
-//
-//     useEffect(() => {
-//         const fetchIcons = async() => {
-//             const icons = await axios.get("http://localhost:8080/lat_long");
-//             setIcons(icons.data);
-//         }
-//         fetchIcons();
-//     }, []);
-//
-//     // add code to get locations 'array' to use at each
-//     return (
-//         <div className="icons">
-//             {icons ?
-//
-//                 icons.map((icon) =>
-//                     <ul key={icon['id']}>
-//                         {icons.map((icon, index) => <img src={icon[index]}/>)}
-//                     </ul>
-//                 ) : null
-//             }
-//         </div>
-//     );
-// }
-// export const Locations = () => {
-//     const [locations, setLocations] = useState([]);
-//
-//     useEffect(() => {
-//         const fetchLocations = async() => {
-//             const locations = await axios.get("http://localhost:8080/location_names");
-//             setLocations(locations.data);
-//         }
-//         fetchLocations();
-//     }, []);
-//
-//     return (
-//         <div className="names">
-//             {locations ?
-//             <ul id="names">
-//                 {locations.map((location: {name: string, id: string}, index) => <li key={index}>{location.name}</li>)}
-//             </ul>
-//                 : null }
-//         </div>
-//
-//     );
-// }
-
-//
-// export const Seeder = () => {
-//     const [seeder, setSeeder] = useState([]);
-//
-//     useEffect(() => {
-//         const seedLocations = async() => {
-//             const seeder = await axios.get("http://localhost:8000/dbSeed/");
-//             setSeeder(seeder.data);
-//         }
-//         seedLocations();
-//     }, []);
-//     return (
-//         <div className="seeder">
-//         </div>
-//     );
-// }
